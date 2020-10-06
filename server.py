2f7a4771d4b28e0d08c302c1e08b6cad7ea18d79
ryanbarclay <mrryanbarclay@gmail.com>
2020-10-06 10:18:50 -0700

    Test to see if backend auto deploys

--- server.py
+++ server.py
@@ -18,10 +18,10 @@ class Battlesnake(object):
         # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "author": "Monty-the-python",
+            "color": "#002b36",  # TODO: Personalize
+            "head": "pixel",  # TODO: Personalize
+            "tail": "pixel",  # TODO: Personalize
         }
 
     @cherrypy.expose
